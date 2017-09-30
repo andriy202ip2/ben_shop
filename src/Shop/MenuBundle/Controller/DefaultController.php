c41dc0b0de51f5b33558e71a55a636ef19f8a487
andriy202ip2 <andriy202ip2@gmail.com>
2017-09-30 03:07:40 +0300

    add jqveru ui

--- src/Shop/MenuBundle/Controller/DefaultController.php
+++ src/Shop/MenuBundle/Controller/DefaultController.php
@@ -3,12 +3,24 @@
 namespace Shop\MenuBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
+    {        
+        //echo rand(1, 100000);
+        echo $request->query->get('id', 0);
+        
+        
+        return $this->render('ShopMenuBundle:Default:index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+    
+    public function testAction()
     {
-        echo rand(1, 100000);
+        echo "rand(1, 100000)";
         return $this->render('ShopMenuBundle:Default:index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
