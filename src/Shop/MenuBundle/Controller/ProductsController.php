c9d330c4ca3b1933313b6efd1bdaa3eaaa012ef4
andriy202ip2 <andriy202ip2@gmail.com>
2018-07-04 06:39:24 +0300

    add user produktion items new logik

--- src/Shop/MenuBundle/Controller/ProductsController.php
+++ src/Shop/MenuBundle/Controller/ProductsController.php
@@ -39,14 +39,51 @@ class ProductsController extends Controller {
         ));
     }
 
-    public function itemsAction($model_id, $auto_id, $data_id) {
-        
+    public function itemsAction($model_id, $auto_id, $data_id, Request $request) {
+
+        $s1 = $request->query->getInt('s1', 0);
+        $s1 = $s1 >= 1 ? 1 : 0;
+
+        $s2 = $request->query->getInt('s2', 0);
+        $s2 = $s2 >= 1 ? 2 : 0;
+
+        $t1 = $request->query->getInt('t1', 0);
+        $t1 = $t1 >= 1 ? 1 : 0;
+
+        $t2 = $request->query->getInt('t2', 0);
+        $t2 = $t2 >= 1 ? 2 : 0;
+
+        $t3 = $request->query->getInt('t3', 0);
+        $t3 = $t3 >= 1 ? 4 : 0;
+
+        $side = ($s1 + $s2) % 3;
+        if (!$side) {
+            $s1 = 1;
+            $s2 = 2;
+        }
+
+        $t = $t1 + $t2 + $t3;
+        if ($t == 0) {
+            $t1 = 1;
+            $t2 = 2;
+            $t3 = 4;
+        }
+        if ($t == 7) {
+            $t = 0;
+        }
+
         $em = $this->getDoctrine()->getManager();
         $ItemsArray = $em->getRepository('ShopMenuBundle:Items')
-                ->findByIdOrderedById($model_id, $auto_id, $data_id, 0);
+                ->findByIdOrderedById($model_id, $auto_id, $data_id, $side, $t, $t1, $t2, $t3)
+                ->getResult();
 
         return $this->render('ShopMenuBundle:Products:items.html.twig', array(
                     'ItemsArray' => $ItemsArray,
+                    's1' => $s1,
+                    's2' => $s2,
+                    't1' => $t1,
+                    't2' => $t2,
+                    't3' => $t3,
         ));
     }
 
